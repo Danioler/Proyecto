2010f8d066b9cce9d58be6840d5b7417c27bdda3
Danioler <144202688+Danioler@users.noreply.github.com>
2025-12-06 23:56:30 -0600

    Create Proyecto Final.cpp

---
+++ Proyecto Final.cpp
@@ -0,0 +1,6 @@
+//Progrma de gestión que vincula robots con sus señales telemáticas mediante archivos binarios para realizar administración de inventario, diagnósticos de fallas y reportes estadísticos
+//Autor: Erik Daniel Rodríguez Ramirez
+//Jose Ramon Chavez Toro
+//Fecha: 11/12/2025
+#include <iostream>
+
