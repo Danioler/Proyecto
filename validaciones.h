980503de75ded4b9228b9ca03ded8ba5d240bbea
Danioler <144202688+Danioler@users.noreply.github.com>
2025-12-11 00:26:56 -0600

    Unificar validaciones.cpp en validaciones.h y actualizar compilacion

--- validaciones.h
+++ validaciones.h
@@ -1,8 +1,27 @@
 #ifndef VALIDACIONES_H
 #define VALIDACIONES_H
 
-// Prototipos de funciones de validacion
-int validarEntero(void);
-float validarFlotante(void);
+#include <iostream>
+using namespace std;
+
+int validarEntero(void){
+    int numero;
+    while(!(cin >> numero)){
+        cout << "ERROR: Debe ingresar un numero entero valido. Intente de nuevo: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    return numero;
+}
+
+float validarFlotante(void){
+    float numero;
+    while(!(cin >> numero)){
+        cout << "ERROR: Debe ingresar un numero decimal valido. Intente de nuevo: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    return numero;
+}
 
 #endif
